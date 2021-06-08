a1ca33ae60920e5ece2e3d53713dccd620d98c65
dvidotti <danilovidotti@gmail>
2021-06-08 22:22:49 +0100

    Allow credentials

--- app.js
+++ app.js
@@ -26,11 +26,11 @@ const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.
 
 const app = express();
 
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use(cors({
   origin: 'https://suspicious-pare-3d027e.netlify.app',
